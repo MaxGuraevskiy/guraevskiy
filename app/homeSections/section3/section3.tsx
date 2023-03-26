b5b7c33875bce4b104de60502e880016eb5282e8
unknown <maximguraevskiy@gmail.com>
2023-03-26 19:57:56 +0300

    change property from transform to translate

--- app/homeSections/section3/section3.tsx
+++ app/homeSections/section3/section3.tsx
@@ -93,9 +93,10 @@ function Section3() {
     // const widths = children.map((e) => e?.clientWidth);
     track.current?.animate(
       {
-        transform: `translate(${
-          (nextPercentage / 100) * children.length * 34
-        }vmin, 0%)`,
+        // transform: `translate(${
+        //   (nextPercentage / 100) * children.length * 34
+        // }vmin, 0%)`,
+        translate: `${(nextPercentage / 100) * children.length * 34}vmin`,
       },
       { duration: 1200, fill: "forwards" }
     );
